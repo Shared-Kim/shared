49371d4951fcf1595392a73a2e5844865fa443f3
zxoo1 <zxoo2546@gmail.com>
2021-12-11 13:45:45 +0900

    react zxoo part add

--- router/zxoo.js
+++ router/zxoo.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const prefixPath = 'react 리소스 폴더 위치'; // react 정적 리소스 폴더 위치 추가
+const prefixPath = '../react_part/zxoo_view/build/'; // react 정적 리소스 폴더 위치 추가
 
-/*라우팅 경로 추가 example
+/*라우팅 경로 추가 example*/
 router.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, prefixPath+'index.html'));
 });
-*/
+
 
 module.exports = router; // 가장 마지막에 사용, 없을 경우 라우팅 되지 않음
